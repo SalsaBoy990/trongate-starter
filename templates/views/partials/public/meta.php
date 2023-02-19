e6f5eb4f3fb34fa553bd7713501e63836dc6690c
Gulácsi András <guland@protonmail.com>
2023-02-20 00:16:39 +0100

    add meta partial for SEO optimization

---
+++ templates/views/partials/public/meta.php
@@ -0,0 +1,19 @@
+    <!-- Metas for social media-->
+    <meta name="description" content="<?= $description ?>"/>
+    <meta property="og:title" content="<?= $title ?>"/>
+    <meta property="og:url" content="<?= current_url() ?>"/>
+    <meta property="og:site_name" content="<?= WEBSITE_NAME ?>"/>
+    <meta property="og:description" content="<?= $description ?>"/>
+    <meta property="og:image" content="<?= BASE_URL.'images/clean-trongate.jpg' ?>"/>
+    <meta property="og:type" content="website"/>
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:site" content="<?= WEBSITE_NAME ?>"/>
+    <meta name="twitter:creator" content="<?= OUR_NAME ?>"/>
+    <meta name="twitter:title" content="<?= $title ?>"/>
+    <meta name="twitter:description" content="<?= $description ?>"/>
+    <meta name="twitter:url" content="<?= current_url() ?>"/>
+    <meta name="twitter:image" content="<?= BASE_URL.'images/clean-trongate.jpg' ?>"/>
+
+    <link rel="canonical" href="<?= current_url() ?>"/>
