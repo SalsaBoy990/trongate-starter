7f0a285cff664fb79e62321c8fe724e5ca178ca6
Gulácsi András <guland@protonmail.com>
2023-03-20 02:56:59 +0100

    fix: require autoloader

--- public/index.php
+++ public/index.php
@@ -10,6 +10,10 @@ require_once '../i18n/i18n.php';
 // General helper functions
 require_once '../helper/helpers.php';
 
+// somewhere early in your project's loading, require the Composer autoloader
+// see: http://getcomposer.org/doc/00-intro.md
+require '../vendor/autoload.php';
+
 // Start the framework
 require_once '../engine/ignition.php';
 
