a9f4f759bb3b85f62802f06ac8e128614078b064
Gulácsi András <guland@protonmail.com>
2023-06-04 22:24:06 +0200

    update: Entries module - add styling, add an image asset

--- modules/entries/views/public/index.php
+++ modules/entries/views/public/index.php
@@ -1,15 +1,16 @@
-<h1>All Entries</h1>
-<nav>
-	<?php if ( count( $entries ) > 0 ) {
-		foreach ( $entries as $entry ) { ?>
-            <h3 class="fs-24"><?= anchor( 'entry/' . $entry->url_string, $entry->title ) ?></h3>
-			<?php
-		}
-	} else { ?>
-        <p>There are no entries yet.</p>
-	<?php } ?>
-</nav>
-
+<div class="entries">
+    <h1>All Entries</h1>
+    <nav>
+		<?php if ( count( $entries ) > 0 ) {
+			foreach ( $entries as $entry ) { ?>
+                <h3 class="fs-24"><?= anchor( 'entry/' . $entry->url_string, $entry->title ) ?></h3>
+				<?php
+			}
+		} else { ?>
+            <p>There are no entries yet.</p>
+		<?php } ?>
+    </nav>
+</div>
 
 
 
