7febabe537cb13d3a26937a8726beeacb6c28cbd
Gulácsi András <guland@protonmail.com>
2023-06-05 03:09:19 +0200

    add: Members module

--- templates/views/partials/admin/dynamic_nav.php
+++ templates/views/partials/admin/dynamic_nav.php
@@ -2,4 +2,6 @@
     <li><?= anchor('entries/manage', '<i class="fa fa-book"></i> Manage Entries') ?></li>
     <li><?= anchor('fish/manage', 'Manage Fish') ?></li>
     <li><?= anchor('fruits/manage', 'Manage Fruits') ?></li>
+
+    <li><?= anchor('members/manage', 'Manage Members') ?></li>
 </ul>
